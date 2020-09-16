261bc7bc9123b56a7dac9d9308731d86e3b74963
kalemsalie <kalemsalie999@gmail.com>
2020-09-16 16:38:31 +0200

    my initial commit

---
+++ hello.py
@@ -0,0 +1,15 @@
+"""
+print("Hello guys ! How are you?")
+print("I live in Cape Town")
+print(5+9)
+
+firstname=input("What is your name")
+print("My name is ", firstname)
+"""
+#==============AREA OF A CIRLCE=======================
+rad = float(input("Enter Radius: \n"))
+
+pi = 3.1415
+
+print(rad * rad * pi)
+
